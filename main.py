e5e71c5bce4663f8930d5b68d0d133e14db99dc3
kirshamir <amirk@mta.ac.il>
2025-07-02 23:08:20 +0300

    Add is_divisible_by function stub to main.py for divisibility checks

--- main.py
+++ main.py
@@ -10,9 +10,21 @@ def is_prime(n):
             return False
     return True
 
+# a function that gets a divisor and returns a function that checks if a number is divisible by that divisor
+def is_divisible_by(divisor):
+    def check_divisibility(n):
+        return n % divisor == 0
+    return check_divisibility
+
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     if is_prime(num):
         print(f"{num} is a prime number.")
     else:
         print(f"{num} is not a prime number.")
+    for i in range(2, int(num/2) + 1):
+        divisible_by_i = is_divisible_by(i)
+        if divisible_by_i(num):
+            print(f"{num} is divisible by {i}.")
+        else:
+            print(f"{num} is not divisible by {i}.")
